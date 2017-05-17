123de70cd44e217fca90aa94b52228ff6993d3a6
Sun Woo Kim <kimsunw@flash.soic.indiana.edu>
2017-05-16 20:24:49 -0400

    Skeleton for stack and queue added

---
+++ nodes_trees/queue.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from node import Node
+from testdoublylinkedlist import DoublyLinkedList
+
+class Queue(DoublyLinkedList):
+
+    def empty(self):
+        pass
+
+    def push(self, data):
+        pass
+
+    def pop(self):
+        pass
---
+++ nodes_trees/stack.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from node import Node
+from testsinglylinkedlist import SinglyLinkedList
+
+class Stack(SinglyLinkedList):
+
+    def empty(self):
+        pass
+
+    def push(self, data):
+        pass
+
+    def pop(self):
+        pass
