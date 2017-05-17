cde46c7acc0a5ab1e306cb29e9e5207b81a6ca4d
Sun Woo Kim <kimsunw@flash.soic.indiana.edu>
2017-05-17 19:34:01 -0400

    Skeleton for BST added

---
+++ nodes_trees/bst.py
@@ -0,0 +1,19 @@
+from treenode import Treenode
+
+class BinarySearchTree():
+    def __init__(self):
+        self.root = None
+
+    def insert(self, node):
+        pass
+
+    def search(self, node):
+        pass
+
+    def delete(self, node):
+        pass
+
+    def find_min(self):
+        pass
+
+    # Some traversals
